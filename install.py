f13b0e4acc70da06123a04098ffd98afad7c88b3
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2024-01-05 01:54:11 +0300

    install.py

--- install.py
+++ install.py
@@ -1,7 +1,31 @@
 import os
+import random
+from termcolor import colored
+os.system("clear")
 
-os.system("pip install pyfiglet")
-os.system("pip install colorama")
+text = '''
+██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     
+██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     
+██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     
+██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝
 
-print("تم التحميل")
+'''
 
+colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan']
+
+random_color = random.choice(colors)
+
+colored_text = colored(text, random_color)
+print(colored_text)
+
+libraries_to_install = [
+    "colorama",
+    "Fore",
+    "Style",
+    "termcolor"
+]
+
+for library in libraries_to_install:
+    os.system(f"pip install {library}")
