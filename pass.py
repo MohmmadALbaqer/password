41f3b3edd39246e70c70f48934879a8c5f5ef538
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2024-04-14 10:13:37 +0300

    Update PS.py

--- pass.py
+++ pass.py
@@ -1,32 +1,44 @@
 import random
 import os  
-from colorama import Fore, Style, init
-from termcolor import colored
+from colorama import Fore, Style, Back, init
 
-os.system("clear")
+init()
 
-text = '''
- ▄▄▄· ▄▄▄· .▄▄ · .▄▄ · ▄▄▌ ▐ ▄▌      ▄▄▄  ·▄▄▄▄    .▄▄ · ▄▄▄ . ▄▄· ▄• ▄▌▄▄▄  ▪  ▄▄▄▄▄ ▄· ▄▌
-▐█ ▄█▐█ ▀█ ▐█ ▀. ▐█ ▀. ██· █▌▐█ ▄█▀▄ ▀▄ █·██· ██   ▐█ ▀. ▀▄.▀·▐█ ▌▪█▪██▌▀▄ █·██ •██  ▐█▪██▌
- ██▀·▄█▀▀█ ▄▀▀▀█▄▄▀▀▀█▄██▪▐█▐▐▌▐█▌.▐▌▐▀▀▄ ▐█▪ ▐█▌  ▄▀▀▀█▄▐▀▀▪▄██ ▄▄█▌▐█▌▐▀▀▄ ▐█· ▐█.▪▐█▌▐█▪
-▐█▪·•▐█▪ ▐▌▐█▄▪▐█▐█▄▪▐█▐█▌██▐█▌▐█▌.▐▌▐█•█▌██. ██   ▐█▄▪▐█▐█▄▄▌▐███▌▐█▄█▌▐█•█▌▐█▌ ▐█▌· ▐█▀·.
-.▀    ▀  ▀  ▀▀▀▀  ▀▀▀▀  ▀▀▀▀ ▀▪ ▀█▄▀▪.▀  ▀▀▀▀▀▀•    ▀▀▀▀  ▀▀▀ ·▀▀▀  ▀▀▀ .▀  ▀▀▀▀ ▀▀▀   ▀ • 
+R = f"{Fore.RED}{Style.BRIGHT}"
+G = f"{Fore.GREEN}{Style.BRIGHT}"
+B = f"{Fore.BLUE}{Style.BRIGHT}"
+Y = f"{Fore.YELLOW}{Style.BRIGHT}"
+C = f"{Fore.CYAN}{Style.BRIGHT}"
+M = f"{Fore.MAGENTA}{Style.BRIGHT}"
+W = f"{Fore.WHITE}{Style.BRIGHT}"
+D = f"{Fore.BLACK}{Style.BRIGHT}"
+ERROR = f"{Y}[{R}!{Y}]{R}"
 
-'''
+def clear_screen():
+    operating_system = os.name
+    try:
+        if operating_system == 'posix': 
+            os.system('clear')
+        elif operating_system == 'nt': 
+            os.system('cls')
+        else:
+            print(f"[!] System unknown!{Style.RESET_ALL}")
+    except Exception as e:
+        print(f"[ERROR]{W}: {e}")
+clear_screen()
 
-colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
-
-random_color = random.choice(colors)
-
-colored_text = colored(text, random_color)
-print(colored_text)
-
-print(f'''
- {Fore.RED}<--------------------------------------------------------------------->
- {Fore.RED}|{Fore.GREEN} GitHub{Fore.WHITE} : {Fore.BLUE}MohmmadALbaqer {Fore.WHITE}|{Fore.YELLOW}   https://www.github.com/MohmmadALbaqer/  {Fore.RED}|
- {Fore.RED}|{Fore.GREEN} Instagram{Fore.WHITE} :{Fore.BLUE} r94xs {Fore.WHITE}      |{Fore.YELLOW}   https://www.instagram.com/r94xs/        {Fore.RED}|
- {Fore.RED}+---------------------------------------------------------------------+
-{Style.RESET_ALL}''')
+print(f"""
+{Y}                                                                              
+ _____                             _    _____                   ___    _       
+|  _  |___ ___ ___ _ _ _ ___ ___ _| |  |   __|___ ___ _ _ ___  __H__ _| |_ _ _ 
+|   __| .'|_ -|_ -| | | | . |  _| . |  |__   | -_|  _| | |  _|  [{Back.RED}{Y}P{Style.RESET_ALL}{Y}] |_   _| | |
+|__|  |__,|___|___|_____|___|_| |___|  |_____|___|___|___|_|    [{Back.RED}{Y}+{Style.RESET_ALL}{Y}]   | | |_  |
+                                                                [{Back.RED}{Y}S{Style.RESET_ALL}{Y}]   |_| |___|
+                                                                 V    
+{R}+------------------------------------------------------------------+
+{R}|{G} GitHub{W} : {B}MohmmadALbaqer {W}|{Y} https://www.github.com/MohmmadALbaqer/ {R}|
+{R}|{G} Instagram{W} :{B} r94xs {W}      |{Y} https://www.instagram.com/r94xs/       {R}|
+{R}+------------------------------------------------------------------+{W}""")
 
 lower = "abcdefghijklmnopwrstuvwxyz"
 upper = "ABCEFGHIJKLMNOPQRSTUVWXYZ"
@@ -41,24 +53,24 @@ def generate_password(length):
 
 while True:
     try:
-        length = int(input(f"{Fore.GREEN}[+] {Fore.BLUE}Enter how long you want the code {Fore.YELLOW}[5 - 25] :{Style.RESET_ALL}"))
+        length = int(input(f"{B}[{G}+{B}] {G}Enter how long you want the code {Y}[5 - 25] : {B}"))
     except ValueError:
-        print(f"{Fore.YELLOW}[{Fore.RED}!{Fore.YELLOW}] {Fore.RED}Please enter the correct code.{Style.RESET_ALL}")
+        print(f"{Y}[{R}!{Y}] {R}Please enter the correct code.{W}")
         continue
 
     if length < 5:
-        print(f"{Fore.RED}[!] Please enter a number greater than {Fore.YELLOW}4{Fore.WHITE}.{Style.RESET_ALL}")
+        print(f"{Y}[{R}!{Y}] {R}Please enter a number greater than {Y}4{W}.")
         continue
 
     password = generate_password(length)
     
     if length <= 25:
-        print(Fore.GREEN + "[*] Password: " + password + Style.RESET_ALL)
+        print(f"{G}[{B}*{G}] {M}Password: {G}" + password + W)
     else:
-        print(Fore.RED + "[*] Password: " + password + Style.RESET_ALL)
+        print(f"{Y}[{R}*{Y}] {M}Password: {R}" + password + W)
 
-    response = input("[?] Do you want to try? (y/n): ")
+    response = input(f"{C}[{M}?{C}] {G}Do you want to try? (y/n): {W}")
 
     if response.lower() != "y":
         break
-print(f"{Fore.BLUE}[+] {Fore.WHITE}Thank you")
+print(f"{G}[{B}*{G}] {W}Thank you")
